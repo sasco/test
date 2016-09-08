0aab3db51395e1be72506f0088d4bd4f45c945eb
Sara <sara@mavenhive.in>
2016-09-08 17:51:14 +0530

    Destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,13 @@ class BooksController <  ApplicationController
     redirect_to books_path
   end
 
+  def destroy
+  @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+
+  end
+
   def new
     @book = Book.new
   end
