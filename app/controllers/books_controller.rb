b496eac3cc4598a4c2acd720d1b1e08904bd8fa8
Sachin Sudheendra <sachin.sudheendra@gmail.com>
2016-09-14 16:13:25 +0530

    Adding before filter. Added icon

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,34 +1,9 @@
 class BooksController <  ApplicationController
-
   before_action :authenticate_user!
+  before_filter :load_book, except: [:index, :new]
 
   def index
-    @books=current_user.books
-  end
-
-  def show
-  @book = Book.find(params[:id])
-  end
-
-  def edit
-    @book = Book.find(params[:id])
-  end
-
-  def update
-    @book = Book.find(params[:id])
-    success = @book.update(book_params)
-    @book.author.update(author_params)
-    if !success
-      render :edit and return
-    end
-    redirect_to books_path
-  end
-
-  def destroy
-  @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
-
+    @books = current_user.books
   end
 
   def new
@@ -42,14 +17,28 @@ class BooksController <  ApplicationController
     redirect_to books_path
   end
 
-  def author_params
-    params.require(:book).permit(author: [:name])[:author]
+  def update
+    render :edit and return unless @book.update(book_params.merge({author_attributes: author_params}))
+    redirect_to books_path
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
   end
 
   private
+
+  def author_params
+    params.require(:book).permit(author: [:name])[:author]
+  end
 
   def book_params
     params.require(:book).permit(:name, :year, :isbn)
   end
 
+  def load_book
+    @book = current_user.books.find(params[:id])
+    @book
+  end
 end
